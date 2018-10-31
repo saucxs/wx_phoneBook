5d177bc6f12d53a5ef299d975b1ce544837f1684
saucxs <saucxs@163.com>
2018-10-31 23:20:08 +0800

    update

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,56 @@
-// pages/user/user.js
-Page({
+const app = getApp();
+const filter = require('../../utils/filter');
+Page(filter.loginCheck({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		userID: '',
+		user: {}
+	},
 
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+		this.setData({
+			userID: wx.getStorageSync('USERID')
+		});
+		let that = this;
+		wx.request({
+			url: `${app.globalData.apiUrl}/getContactByID/${that.data.userID}`,
+			data: {},
+			method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+			// header: {}, // 设置请求的 header
+			success: function (res) {
+				// success
+				if (res.data.success) {
+					that.setData({
+						user: res.data.data
+					})
+				}
+			},
+			fail: function () {
+				// fail
+			},
+			complete: function () {
+				// complete
+			}
+		})
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+	callPhone: function (e) {
+		wx.makePhoneCall({
+			phoneNumber: e.currentTarget.dataset.phone
+		})
+	},
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+		return {
+			title: `${this.data.user.name}的名片`
+		}
+	}
+}))
