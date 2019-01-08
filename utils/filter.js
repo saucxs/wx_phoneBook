324ceb468ff69782106466cfbb9a7a18ea85a035
saucxs <saucxs@163.com>
2019-01-08 09:40:58 +0800

    add filter

---
+++ utils/filter.js
@@ -0,0 +1,26 @@
+function loginCheck(pageObj) {
+  if (pageObj.onLoad) {
+    let _onLoad = pageObj.onLoad;
+    pageObj.onLoad = function (options) {
+      if (wx.getStorageSync('USERID')) {
+        //获取页面实例，防止this劫持
+        let currentInstance = getPageInstance();
+        _onLoad.call(currentInstance, options);
+
+      } else {
+        //跳转到登录页
+        wx.redirectTo({
+          url: "/pages/login/login"
+        });
+      }
+    }
+  }
+  return pageObj;
+}
+
+function getPageInstance() {
+  var pages = getCurrentPages();
+  return pages[pages.length - 1];
+}
+
+exports.loginCheck = loginCheck;
